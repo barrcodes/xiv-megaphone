707270bc023431e21075550b301dc813db2ae6ee
barrcodes <1237457+barrcodes@users.noreply.github.com>
2026-05-20 23:37:21 -0400

    # Features:
    - Added non-premium defaults for people using their own inworld API keys
    
    # Maintenance:
    
    - Removed bin, as the web audio api is only available in the electron web app
    
    - Removed unused ffplay audio player

--- frontend/src/shared/defaults.ts
+++ frontend/src/shared/defaults.ts
@@ -3,9 +3,73 @@ import type { Preset } from "./types";
 export const DEFAULT_PORT = 57575;
 export const DEFAULT_MODEL = "inworld-tts-1.5-mini";
 
+/**
+ * As reasonable of defaults as I could come up with from Inworld's built-in voices.
+ */
 export const DEFAULT_PRESET: Preset = {
   id: "default",
   name: "Default",
+  isDefault: true,
+  male: "Graham",
+  female: "Wendy",
+  default: "Luna",
+  speakingRate: 1.25,
+  namedVoices: {
+    "hyur male": "Graham",
+    "hyur female": "Wendy",
+    "elezen male": "Clive",
+    "elezen female": "Claire",
+    "lalafell male": "Abby",
+    "lalafell female": "Pixie",
+    "miqo'te male": "Satoshi",
+    "miqo'te female": "Jing",
+    "roegadyn male": "Hades",
+    "roegadyn female": "Victoria",
+    "hrothgar male": "Malcom",
+    "hrothgar female": "Veronica",
+    "viera male": "Seojun",
+    "viera female": "Yoona",
+    "au ra male": "Nikolai",
+    "au ra female": "Elena",
+
+    // Beast Tribes
+    "amalj'aa": "Hades",
+    sylph: "Pixie",
+    kobold: "Mortimer",
+    sahagin: "Lupita",
+    ixal: "Malcolm",
+    vanu_vanu: "Wendy",
+    gnath: "Minji",
+    moogle: "Pixie",
+    kojin: "Hades",
+    ananta: "Lupita",
+    namazu: "Pixie",
+    pixie: "Pixie",
+    qiqirn: "Mortimer",
+    matanga: "Hades",
+    omicron: "Dominus",
+    goblin: "Mortimer",
+    mamool_ja: "Malcolm",
+    dragon: "Hades",
+    lupin: "Hades",
+    bangaa: "Malcolm",
+    seeq: "Hades",
+    nu_mou: "Abby",
+    loporrit: "Pixie",
+    ea: "Luna",
+    karellian: "Dominus",
+    grebuloff: "Pixie",
+    nibirun: "Dennis",
+    gordhonite: "Dominus",
+  },
+};
+
+/**
+ * Requires subscription due to custom voices only available in our workspace
+ */
+export const PREMIUM_DEFAULT_PRESET: Preset = {
+  id: "default-premium",
+  name: "Default Premium",
   isDefault: true,
   male: "Graham",
   female: "Wendy",
